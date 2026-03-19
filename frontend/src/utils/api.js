811891b929e7829c27b652b98288f44652483211
vamshikishore613-byte <vamshikishore613@gmail.com>
2026-03-19 19:09:17 +0530

    Fix API URL and CORS

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_BASE = 'https://cybershield-backend-new.onrender.com/api';
+```
 
 const api = axios.create({ baseURL: API_BASE });
 
